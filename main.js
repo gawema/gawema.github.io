32dda2283260855edc729cfdbf2351f3c631d8e1
gawema <gab.mannu@hotmail.it>
2020-04-30 19:38:43 +0200

    Grammar fix.

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function toggleMenu() {
 const BLACKLISTED_KEY_CODES = [ 38 ];
 const COMMANDS = {
   pwd:
-    `Hei 👋 Welcome to my website, this are the supported commands:</br>
+    `Hei 👋 Welcome to my website, these are the supported commands:</br>
     about, experience, education, skills, contact, cv `,
   ls:
     `
@@ -28,15 +28,15 @@ const COMMANDS = {
     'Supported commands: <br> about, experience, education, skills, contact, cv',
   about:
     `Ciao, I'm Gabriele, <br>
-    I am a Computer Science student, Full-stack developer and IT enthusiast. 
-    I am currently based in copenhagen and enrolled in a computer science bachelor at the Copenhagen School of Design and Technology.
+    I am a Computer Science student, full-stack developer and IT enthusiast. 
+    I am currently based in Copenhagen and enrolled in a Computer Science Bachelor at the Copenhagen School of Design and Technology.
     `,
   experience:
     `
     </br> <strong>MANCOFI</strong> </br>
     Currently working as a junior full-stack developer at <a href='https://mancofi.dk/' target="_blank" style='color: white'>MANCOFI</a>. </br>
     Here, my main focus is to develop b2b products together with a team of experienced developers and consultants.</br>
-    I Primarily work with Angular on the front-end and .Net on the back-end. I also get to work with various DevOps technologies, such as:</br>
+    I primarily work with Angular on the front-end and .Net on the back-end. I also get to work with various DevOps technologies, such as:</br>
     Azure, Docker & Kubernetes. </br> 
     Finally, since I am very passionate about "how things look" I also get assigned various design tasks, where I get to work with a vast range of UI/UX softwares.
     </br> </br> <strong>Plant Jammer</strong> </br> 
@@ -45,8 +45,8 @@ const COMMANDS = {
     `,
   education:
     `
-      <strong>2019-2021: </strong> Bachelor degree in web development at <a href='https://kea.dk/en' target="_blank" style='color: white'>KEA</a>. </br>
-      <strong>2016-2019: </strong> Academy profession degree in computer science at <a href='https://kea.dk/en' target="_blank" style='color: white'>KEA</a>. </br>
+      <strong>2019-2021: </strong> Bachelor degree in Web Development at <a href='https://kea.dk/en' target="_blank" style='color: white'>KEA</a>. </br>
+      <strong>2016-2019: </strong> Academy profession degree in Computer Science at <a href='https://kea.dk/en' target="_blank" style='color: white'>KEA</a>. </br>
       <strong>2011-2016: </strong> International relations and marketing at <a href='http://www.peano.edu.it' target="_blank" style='color: white'>G.Peano</a> High School in Florence, Italy.
     `,
   skills:
